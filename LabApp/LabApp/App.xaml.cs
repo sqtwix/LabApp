9352009ae41dc3757e8b98e910b242568d20432d
sqtwix <ivan20140767@gmail.com>
2026-05-10 17:11:23 +0300

    feat: DI config in Prezentation layer

--- LabApp/LabApp/App.xaml.cs
+++ LabApp/LabApp/App.xaml.cs
@@ -1,14 +1,71 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using LabApp.Application.Interfaces;
+using LabApp.Infrastructure.Services;
+using LabApp.Infrustructure;
+using LabApp.Infrustructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
-namespace LabApp
+namespace LabApp;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : System.Windows.Application
+    private IServiceProvider _serviceProvider;
+    private IConfiguration _configuration;
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        _configuration = builder.Build();
+
+        var services = new ServiceCollection();
+
+        RegistrateDbContext(services);
+
+        RegistrateServices(services);
+
+        RegistrateViewModels(services);
+
+        _serviceProvider = services.BuildServiceProvider();
+
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
     }
 
+    // Registration db context
+    private void RegistrateDbContext(IServiceCollection services)
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<LabContext>(options =>
+            options.UseNpgsql(connectionString));
+    }
+
+    // Services Registration for DI
+    private void RegistrateServices(IServiceCollection services) 
+    {
+        services.AddTransient<IPatientService, PatientService>();
+        services.AddTransient<IAppointmentService, AppointmentService>();
+        services.AddTransient<IResearchService, ResearchService>();
+        services.AddTransient<IStaffService, StaffService>();
+        services.AddTransient<IEquipmentService, EquipmentService>();
+        services.AddTransient<IReportingService, ReportingService>();
+        services.AddTransient<IAdminService, AdminService>();
+    }
+
+    // ViewModel Registration for MVVM
+    private void RegistrateViewModels(IServiceCollection services)
+    {
+        services.AddTransient<MainWindow>();
+    }
 }
+
+
